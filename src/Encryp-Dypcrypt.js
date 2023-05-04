94898ab15d83fef4fb34e6f1afbce89a2627ecf6
Salahin <rocky.bgta@gmail.com>
2023-05-04 12:34:52 +0600

    java pojo encryption

--- src/Encryp-Dypcrypt.js
+++ src/Encryp-Dypcrypt.js
@@ -11,11 +11,10 @@ console.log(encrypted);
 
 
 
-var decrypted = ch.decrypt(encrypted);
-console.log(decrypted);
-
-/*
-const javaEncrypted = 'QHdt/CvNQOw/M0KUUCcZaDEFnZ7LkKTYhktUv+pXUYPOh14q4UYsD5Odz02C/p8bx8RiTfvXMQQ3xDqMiZoWgo1GMRxEqSjJxxCnZYba3E5Nt7ij3TFODIP6/V5hkRMRivLGz93YykT81B1jNtm3lBkwis6peJfLQZ5ISrmPLAFzYCYSB6Q3NiK76UnEO0dd2WueiylGnaD1EaUyZQ1sb5VPcFEw28fg6q8oridBB9ibBU8rt+tcJGlzNMtwprwPzGvpGCq7uo+EOc7dBpqdZ/UPJMlkMXiorZ2Sms6mkprYBEPOl3VKPA3DgbO7QX504a0RKAyUXJomw06ewjbxAw==';
-
-var decrypted = ch.decrypt(javaEncrypted);
+/*var decrypted = ch.decrypt(encrypted);
 console.log(decrypted);*/
+
+
+const javaEncrypted = 'cIIQCB0GnCDmySuRSK15RGEP/ziZWQE1j+rvyQZbFxxFR1MciLkTAa1+68KC4g1l0cX11GfdOdv5ID2DBZvroVmzKWvdaT+NUUQDzyrTmMOwMW90jWkqDz1zVveShzg8ARPyfUU4fSgqjzmAK/1pNHe+XaepI8lY5BlSUzmFuxQ+Yybggz/CBy0pbIcuHBvyHV55zMCrq8YXt/4QAEeRLVxhJUsJY5JQoMjTuGoJXezvdF2qjsykqUOKuDwJY3LbZ8BwKyAIijGDhqDzCKF1gB8fRr9A1KmpXli4g5UhUIzxDmET9kpW1KHtpfq4LKIErnulIeHXTi3RV+i7A3HbNg=='
+var decrypted = ch.decrypt(javaEncrypted);
+console.log(decrypted);
